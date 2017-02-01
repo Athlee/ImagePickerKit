f467a4519887337eaad216ff92e05d32a388e6d2
Eugene Mozharovsky <mozharovsky@live.com>
2017-02-01 04:38:53 +0300

    Save taken images to albums #6

--- Source/PhotoCapturable.swift
+++ Source/PhotoCapturable.swift
@@ -7,35 +7,44 @@
 //
 
 import UIKit
+import Photos
 import AVFoundation
 
 ///
 /// Provides still image capturing features.
 ///
 public protocol PhotoCapturable: Capturable {
+  
+  /// Captures a still image from the current input.
   ///
-  /// Captures a still image from the current input. 
+  /// - Parameters:
+  ///   - saving:  Indicates if taken image should be saved to albums.
+  ///   - handler: A handler that is called when the image is taken. Default value is `nil`.
+  func captureStillImage(saving: Bool, handler: ((UIImage) -> Void)?)
+  
+  /// This function is optional. It is called when
+  /// captured still image could not
+  /// be saved to albums.
   ///
-  /// - parameter handler: A handler that is called when the image is taken. Default value is `nil`.
-  ///
-  func captureStillImage(_ handler: ((UIImage) -> Void)?)
+  /// - Parameter error: Photo Library change error.
+  func captureStillImageFailed(with error: Error?)
 }
 
 // MARK: - Default implementations 
 
 public extension PhotoCapturable {
   
-  ///
   /// Captures a still image from the current input.
   ///
-  /// - parameter handler: A handler that is called when the image is taken. Default value is `nil`.
-  ///
-  func captureStillImage(_ handler: ((UIImage) -> Void)? = nil) {
+  /// - Parameters:
+  ///   - saving:  Indicates if taken image should be saved to albums.
+  ///   - handler: A handler that is called when the image is taken. Default value is `nil`.
+  func captureStillImage(saving: Bool = true, handler: ((UIImage) -> Void)?) {
     guard let imageOutput = imageOutput else {
       return
     }
     
-    
+    // TODO: Refactor this code 
     
     DispatchQueue.global(qos: DispatchQoS.userInitiated.qosClass).async(execute: {
       let videoConnection = imageOutput.connection(withMediaType: AVMediaTypeVideo)
@@ -61,8 +70,18 @@ public extension PhotoCapturable {
           )
           
           DispatchQueue.main.async {
-            let resizedImage = UIImage(cgImage: imageRef!, scale: sw/iw, orientation: image.imageOrientation)
+            let resizedImage = UIImage(cgImage: imageRef!, scale: sw / iw, orientation: image.imageOrientation)
             handler?(resizedImage)
+            
+            if saving {
+              PHPhotoLibrary.shared().performChanges({
+                PHAssetChangeRequest.creationRequestForAsset(from: resizedImage)
+              }, completionHandler: { [weak self] (success, error) in
+                if !success {
+                  self?.captureStillImageFailed(with: error)
+                }
+              })
+            }
             
             self.session     = nil
             self.device      = nil
@@ -75,4 +94,10 @@ public extension PhotoCapturable {
     })
   }
   
+  /// This function is optional. It is called when
+  /// captured still image could not
+  /// be saved to albums.
+  ///
+  /// - Parameter error: Photo Library change error.
+  func captureStillImageFailed(with error: Error?) { }
 }
